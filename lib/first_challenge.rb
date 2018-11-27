0a3c2e7ad86117913055143b60e7fa995c7f73a5
Simon Balean <simonbalean@gmail.com>
2018-11-27 13:50:46 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,7 +16,7 @@ def first_challenge
 contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
-      value.delete do |flavor|
+      value.delete_if 
 
 
   #remember to return your newly altered contacts hash!
